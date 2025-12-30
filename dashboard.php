91049cb1ca2be56766341664c1c5bc96e6923173
a-oirgari <abdrahmanghali1998@gmail.com>
2025-12-30 16:01:08 +0100

    the main files for the liveCoding

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Dashboard</h2>
+<p>
+    <strong>Don't underestimate this dashboard 🤪</strong>
+</p>
+
+<p>Bienvenue </p>
+
+<a href="logout.php">Déconnexion</a>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Login</h2>
+
+
+
+<form method="post">
+    <input type="email" name="email" required>
+    <input type="password" name="password" required>
+    <button>Se connecter</button>
+</form>
+
+</body>
+</html>
